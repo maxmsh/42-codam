30974c9e12d99efd4a2f68a07ed1efe2d9030e7a
Max Mosheuvel <mmosheuv@f0r4s23.codam.nl>
2026-03-18 18:19:17 +0100

    small changes to errorhandling

--- milestone 2/python_modules/module02/ex0/ft_different_errors.cpp
+++ milestone 2/python_modules/module02/ex0/ft_different_errors.cpp
@@ -11,21 +11,25 @@ void	garden_operations(void)
 	try
 	{
 		input = 0;
-		std::cout << "Please enter input: " << std::endl;
+		std::cout << "Please enter input: " << std::endl; // User is prompted to give input
 		std::cin >> input;
 		if (std::cin.fail())
 		{
 			std::cout << "Testing ValueError..." << std::endl;
-			throw std::invalid_argument("Caught ValueError: invalid literal for int()");
-			std::cin.ignore(10000, '\n');
+			throw std::invalid_argument("Caught ValueError: invalid literal for int()"); // If the value the user has put in is invalid, then they will see a TypeError message
+			std::cin.ignore(10, '\n');
 			std::cin.clear();
+			std::cout << "Try again, please. " << std::endl;
+			std::cin >> input;
 		}
 		else if (input == 0)
 		{
 			std::cout << "Testing ZeroDivisionError..." << std::endl;
-			throw std::runtime_error("Caught ZeroDivisionError: division by zero");
+			throw std::runtime_error("Caught ZeroDivisionError: division by zero"); // This is mainly used for testing purposes, but division by 0 should not be possible
 			std::cin.ignore(10000, '\n');
 			std::cin.clear();
+			std::cout << "Try again, please. " << std::endl;
+			std::cin >> input;
 		}
 	}
 	catch(const std::invalid_argument &e)
@@ -35,21 +39,6 @@ void	garden_operations(void)
 	catch(const std::runtime_error &e)
 	{
 		std::cerr << e.what() << '\n';
-	}
-
-	try
-	{
-		std::cout << "Please specify a filename: " << std::endl;
-		std::cin >> filename;
-
-		if (!filename.open())
-		{
-			throw 
-		}
-	}
-	catch
-	{
-
 	}
 }
 
