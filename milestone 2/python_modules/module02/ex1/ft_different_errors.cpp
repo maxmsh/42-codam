f25feda8645882f4df4c50878341b172c8d297f8
Max Mosheuvel <mmosheuv@f0r3s22.codam.nl>
2026-03-23 20:27:10 +0100

    tweaking

--- milestone 2/python_modules/module02/ex1/ft_different_errors.cpp
+++ milestone 2/python_modules/module02/ex1/ft_different_errors.cpp
@@ -11,11 +11,20 @@ void	garden_operations(int num, int divisor, std::string filename)
 		{
 			throw std::invalid_argument("Testing ValueError...\n");
 		}
+		else if (num / divisor == 0)
+		{
+			throw std::logic_error("Testing ZeroDivisionError...\n");
+		}
 	}
 	catch (const std::invalid_argument &e)
 	{
 		std::cout << "Testing ValueError..." << std::endl;
 		std::cout << "Caught ValueError: invalid literal for int()" << std::endl;
+	}
+	catch (const std::logic_error &e)
+	{
+		std::cout << "Testing ZeroDivisionError..." << std::endl;
+		std::cout << "Caught ZeroDivisionError: division by zero" << std::endl;
 	}
 }
 
@@ -23,5 +32,5 @@ int	main(void)
 {
 	std::cout << "=== Garden Error Types Demo ==="
 				<< "\n\n";
-	garden_operations('p', 21, "missing.txt");
+	garden_operations(12, 21, "missing.txt");
 }
