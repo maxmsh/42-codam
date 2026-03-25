cb3efe3daf86d4063c703571c084a971a082ac25
Max Mosheuvel <mmosheuv@f0r4s21.codam.nl>
2026-03-25 07:59:07 +0100

    added some comments

--- milestone 2/python_modules/module02/ex0/ft_first_exception.cpp
+++ milestone 2/python_modules/module02/ex0/ft_first_exception.cpp
@@ -4,7 +4,12 @@
 
 int	check_temperature(std::string temp_str)
 {
-	int	typecasted;
+	int	typecasted; 
+
+	// typecasted is a variable that stores integer (whole number)
+	// data extracted from the string input variable temp_str.
+	// The input data from temp_str gets converted using the stoi method (string to integer).
+	// It has its default value set to 0 to prevent random garbage values. 
 
 	typecasted = 0;
 	std::cout << "\n"
@@ -25,6 +30,14 @@ int	check_temperature(std::string temp_str)
 			std::cout << typecasted << "°C is perfect for plants!" << std::endl;
 		}
 	}
+	
+	// My try block contains some conditional logic; depending on what input is entered,
+	// a different message will be printed to the terminal. 
+	// If the input is somewhere between 40 and 0, it will tell you
+	// that the temperature value you have entered is suitable for plant growth.
+	// If not, then it will either tell you that your input value was
+	// too hot or too cold to foster proper plant growth. 
+
 	catch (const std::invalid_argument &e)
 	{
 		if (std::strcmp(e.what(), "hot") == 0)
@@ -40,6 +53,12 @@ int	check_temperature(std::string temp_str)
 			std::cout << "Error: " << temp_str << " is not a valid number" << std::endl;
 		}
 	}
+
+	// Here we actually catch the errors we saw previously.
+	// I used strcmp to make sure that the error messages were
+	// compared the right way as it would only compare the first
+	// character of the string when I would try it any other way.
+
 	return (typecasted);
 }
 
@@ -55,6 +74,10 @@ void test_temperature_input()
 	check_temperature("-50");
 	std::cout << "\n\n" << "All tests completed - program didn't crash!" << std::endl;
 }
+
+// As per the subject change, 42 now requires a testing function named
+// test_temperature_input() for this exercise. I mainly used this function as a placeholder
+// for a couple of value tests as I couldn't figure out much use for it myself. 
 
 int	main(void)
 {
