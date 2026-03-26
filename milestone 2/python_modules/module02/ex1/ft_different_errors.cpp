7715f88345f3617ea68198769daccd30d58f3696
Max Mosheuvel <mmosheuv@f0r4s16.codam.nl>
2026-03-26 23:07:56 +0100

    getting frustrated with the snake

--- milestone 2/python_modules/module02/ex1/ft_different_errors.cpp
+++ milestone 2/python_modules/module02/ex1/ft_different_errors.cpp
@@ -5,24 +5,35 @@ int	garden_operations(const std::string &input)
 {
 	try
 	{
-		return (std::stoi(input));
+		int number = std::stoi(input);
+		if (!number)
+			throw std::invalid_argument("invalid literal for int()\n");
+		if (number == 0)
+			throw std::runtime_error("division by zero\n");
 	}
 	catch (const std::invalid_argument &e)
 	{
-		std::cout << "Caught ValueError: invalid literal for int()" << std::endl;
+		std::cout << "Caught ValueError: " << e.what() << std::endl;
 	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Caught ZeroDivisionError: " << e.what() << std::endl;
+	}
+	return 0;
 }
 
 void	test_error_types(void)
 {
-	std::cout << "Testing ValueError..."
-				<< "\n";
+	std::cout << "\nTesting ValueError...\n";
 	garden_operations("abc");
+	std::cout << "\nTesting ZeroDivisionError...\n";
+	garden_operations("15");
+	std::cout << "\n";
 }
 
 int	main(void)
 {
 	std::cout << "=== Garden Error Types Demo ==="
-				<< "\n\n";
+				<< "\n";
 	test_error_types();
 }
--- milestone 2/python_modules/module02/ex1/ft_different_errors.py
+++ milestone 2/python_modules/module02/ex1/ft_different_errors.py
@@ -1,23 +1,36 @@
-def garden_operations(input) -> int:
+def garden_operations(value) -> None:
     try:
-        if isinstance(input, int):
-            return input
-        else:
-            return int(input)
-    except ValueError:
-        print("Caught ValueError: invalid literal for int()")
-    except ZeroDivisionError:
-        print("Caught ZeroDivisionError: division by zero")
+        # div = value(int) / 0
+        return int(value)
+    except ValueError as e:
+        print(f"Caught {type(e).__name__}: {e}")
+    # try:
+    #     return div
+    # except ZeroDivisionError as e:
+    #     print(f"Caught {type(e).__name__}: {e}")
+    # try:
+    #     return open(value, 'r')
+    # except FileNotFoundError as e:
+    #     print(f"Caught {type(e).__name__}: {e}")
+    # try:
+    #     d = {}
+    #     print(d[value])
+    # except KeyError as e:
+    #     print(f"Caught {type(e).__name__}: {e}")
 
 
 def test_error_types():
-    print("Testing ValueError...")
+    print("\nTesting ValueError...")
     garden_operations("abc")
-    print("Testing ZeroDivisionError...")
-    garden_operations("0")
+    # print("\nTesting ZeroDivisionError...")
+    # garden_operations(15)
+    # print("\nTesting FileNotFoundError...")
+    # garden_operations('missing.txt')
+    # print("\nTesting KeyError...")
+    # garden_operations('missing_key')
 
 
 if __name__ == "__main__":
     print("=== Garden Error Types Demo ===")
-    print("")
     test_error_types()
+    print("\nAll error types tested succesfully!")
