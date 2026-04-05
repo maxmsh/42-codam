897538309ed12102091c8f1946fe6cc5cfb5f94a
MaxMsh <mosheuvelmax@gmail.com>
2026-04-05 23:44:03 +0200

    c++ version ready as well

--- milestone 2/python_modules/module03/ex0/ft_command_quest.cpp
+++ milestone 2/python_modules/module03/ex0/ft_command_quest.cpp
@@ -2,7 +2,28 @@
 #include <string>
 using namespace std;
 
-int main(int argc, string argv)
+int main(int argc, char **argv)
 {
+    string program_name = "ft_command_quest.cpp";
+    int args_received = argc - 1;
+    int i = 1;
 
+    cout << "=== Command Quest === \n\n";
+    if (argc == 1)
+    {
+        cout << "No arguments provided!\n";
+        cout << "Program name: " << program_name << '\n';
+        cout << "Total arguments: " << argc << endl;
+    }
+    else if (argc > 1)
+    {
+        cout << "Program name: " << program_name << '\n';
+        cout << "Arguments received: " << args_received << '\n';
+        while (i < argc)
+        {
+            cout << "Argument " << i << ": " << argv[i] << '\n';
+            i++;
+        }
+        cout << "Total arguments: " << argc << '\n';
+    }
 }
