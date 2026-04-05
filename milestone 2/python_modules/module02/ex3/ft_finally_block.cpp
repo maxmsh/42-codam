548129af7af2de836cbdf2202654dd50ccf033c7
MaxMsh <mosheuvelmax@gmail.com>
2026-04-05 21:04:24 +0200

    uploading prototype

--- milestone 2/python_modules/module02/ex3/ft_finally_block.cpp
+++ milestone 2/python_modules/module02/ex3/ft_finally_block.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
 using namespace std;
+
+class Error : public exception
+{
+    public:
+    string message;
+
+    Error(const string &message)
+    : message(message) {}
+
+    const char* what() const noexcept override
+    {
+        return message.c_str();
+    }
+};
+
+struct Plant
+{
+    string name;
+    int water, sun;
+};
+
+void test_watering_system(int errors)
+{
+    cout << "=== Garden Watering System ===\n\n";
+
+    vector<Plant> plants = {
+        {"tomato", 4, 6}
+    };
+
+    
+}
 
 int main(void)
 {
     cout << "=== Garden Watering System ===\n";
-
+    test_watering_system(0);
+    test_watering_system(1);
     cout << "\nCleanup always happens, even with errors!" << endl;
 }
