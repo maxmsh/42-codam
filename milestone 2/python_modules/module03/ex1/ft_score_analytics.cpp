2d65db286d02cc245078d146f4c0412718dec135
MaxMsh <mosheuvelmax@gmail.com>
2026-04-06 21:47:13 +0200

    progressing

--- milestone 2/python_modules/module03/ex1/ft_score_analytics.cpp
+++ milestone 2/python_modules/module03/ex1/ft_score_analytics.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     int i = 1;
+    int total = 0; 
 
     cout << "=== Player Score Analytics ===\n\n";
     try
@@ -14,15 +15,18 @@ int main(int argc, char **argv)
             cout << "Scores processed: [";
             while (argc > i)
             {
-                cout << argv[i];
+                cout << "'" << argv[i] << "'";
                 i++;
                 if (i < argc)
                     cout << ", ";
                 else if (i == argc)
                     cout << "]";
             }
-            cout << "\nTotal players: " << argc - 1 << '\n';
+            cout << "\nTotal players: " << argc - 1;
+            cout << "\nTotal score: " << total << '\n';
         }
+        else if (argc < 2)
+            cout << "No scores provided. Usage: clang++ " << argv[0] << " <score1> <score2>" << endl;
     }
     catch(const std::exception& e)
     {
