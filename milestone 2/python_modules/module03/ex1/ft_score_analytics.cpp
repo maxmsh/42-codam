0c46a2fde2ad5bb4fa3ec02ed3f8452b262ffc32
MaxMsh <mosheuvelmax@gmail.com>
2026-04-15 01:22:48 +0200

    working  from home

--- milestone 2/python_modules/module03/ex1/ft_score_analytics.cpp
+++ milestone 2/python_modules/module03/ex1/ft_score_analytics.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <string>
+#include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char **argv)
 {
     int i = 1;
-    int total = 0; 
+    int players = argc - 1;
+    int total = 0;
 
-    cout << "=== Player Score Analytics ===\n\n";
+    cout << "=== Player Score Analytics ===\n";
     try
-    {
-        if (argc >= 1)
+    {        
+        if (players >= 2)
         {
             cout << "Scores processed: [";
             while (argc > i)
             {
-                cout << "'" << argv[i] << "'";
+                cout << argv[i];
                 i++;
                 if (i < argc)
                     cout << ", ";
                 else if (i == argc)
                     cout << "]";
             }
-            cout << "\nTotal players: " << argc - 1;
+            cout << "\nTotal players: " << players;
+            
             cout << "\nTotal score: " << total << '\n';
         }
-        else if (argc < 2)
+        else
             cout << "No scores provided. Usage: clang++ " << argv[0] << " <score1> <score2>" << endl;
     }
     catch(const std::exception& e)
--- milestone 2/python_modules/module03/ex1/ft_score_analytics.py
+++ milestone 2/python_modules/module03/ex1/ft_score_analytics.py
@@ -2,28 +2,40 @@ import sys
 
 
 if __name__ == "__main__":
-
     program_name = sys.argv[0]
-    scores = list(map(int, sys.argv[1:]))
+
+    scores = []
+    invalid_found = False
+
+    for x in sys.argv[1:]:
+        try:
+            scores.append(int(x))
+        except ValueError:
+            print(f"Invalid parameter: '{x}'")
+            invalid_found = True
+    
+    if invalid_found:
+        print(f"No scores provided. Usage: python3 {program_name} <score1> <score2> ...")
+        sys.exit(1)
+
+
     players = len(scores)
+
+    if players < 2:
+        print(f"No scores provided. Usage: python3 {program_name} <score1> <score2> ...")
+        sys.exit(1)
+    
     total = sum(scores)
     average = total / players
     maximum = max(scores)
     minimum = min(scores)
-    range = maximum - minimum
+    score_range = maximum - minimum
 
     print("=== Player Score Analytics ===")
-    try:
-        if players >= 2:
-            print(f"Scores processed: {scores}")
-            print(f"Total players: {players}")
-            print(f"Total score: {total}")
-            print(f"Average score: {average}")
-            print(f"High score: {maximum}")
-            print(f"Low score: {minimum}")
-            print(f"Score range: {range}")
-        else:
-            print(f"No scores provided. Usage: python3 {program_name} <score1> <score2>")
-    except:
-        for score in scores:
-            raise ValueError(f"Invalid parameter: {score}")
+    print(f"Scores processed: {scores}")
+    print(f"Total players: {players}")
+    print(f"Total score: {total}")
+    print(f"Average score: {average}")
+    print(f"High score: {maximum}")
+    print(f"Low score: {minimum}")
+    print(f"Score range: {score_range}")
