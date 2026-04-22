313012158294e5f8a911c2dd7be5060d2035dcb7
MaxMsh <mosheuvelmax@gmail.com>
2026-04-22 05:26:52 +0200

    working on cpp implementation

--- milestone 2/python_modules/module03/ex3/ft_achievement_tracker.cpp
+++ milestone 2/python_modules/module03/ex3/ft_achievement_tracker.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Player
 {
     public:
-    Player(const string &name, const string &achievements)
-    : name(name), achievements(achievements) {}
+        string name;
+        string achievement;
+
+    Player(string n, string a) : name(n), achievement(a) {}
 };
+
+
+vector <Player> gen_player_achievements()
+{
+    vector <string> achievements = {
+        "Crafting Genius", "Strategist", "World Savior", "Speed Runner", "Survivor",
+        "Master Explorer", "Treasure Hunter", "Unstoppable", "First Steps",
+        "Collector Supreme", "Untouchable", "Sharp Mind", "Boss Slayer", "Hidden Path Finder"
+    };
+
+    int max = achievements.size();
+
+    vector <Player> players = {
+        Player("Alice", achievements[rand() % max]),
+        Player("Bob", achievements[rand() % max]),
+        Player("Charlie", achievements[rand() % max]),
+        Player("Dylan", achievements[rand() % max])
+    };
+
+    return players;
+}
 
 int main(void)
 {
+    srand(time(0));
 
+    vector <Player> players = gen_player_achievements();
+
+    for (Player p: players)
+    {
+        cout << "Player " << p.name << " has achievement: " << p.achievement << endl; 
+    }
+    return 0;
 }
