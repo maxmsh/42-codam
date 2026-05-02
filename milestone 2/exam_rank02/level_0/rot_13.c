44446280145a446b9b58ed4fa69e440b6f7637d7
MaxMsh <mosheuvelmax@gmail.com>
2026-05-03 00:10:05 +0200

    rot13 now also solved

---
+++ milestone 2/exam_rank02/level_0/rot_13.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void rot_13(char *str)
+{
+    int i;
+    i = 0;
+
+    while (str[i] != '\0')
+    {
+        if ((str[i] >= 65 && str[i] <= 77) || (str[i] >= 97 && str[i] <= 109))
+            str[i] = str[i] + 13;
+        else if ((str[i] >= 77 && str[i] <= 90) || (str[i] >= 109 && str[i] <= 122))
+            str[i] = str[i] - 13;
+        write(1, &str[i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+        rot_13(argv[1]);
+    else
+        write(1, "\n", 1);
+    
+    return (0);
+}
