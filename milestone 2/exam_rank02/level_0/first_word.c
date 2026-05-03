26e3640811db8a2f2183b22b62bb40379f3c8ee7
MaxMsh <mosheuvelmax@gmail.com>
2026-05-03 04:22:15 +0200

    improved first_word to working version, refinement will follow soon

--- milestone 2/exam_rank02/level_0/first_word.c
+++ milestone 2/exam_rank02/level_0/first_word.c
@@ -7,13 +7,12 @@ void first_word(char *str)
 
     while (str[i] != '\0')
     {
-        if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
-        {
-            write(1, &str[i], 1);
+        if (((str[i] == 32)) && !((str[i - 1] >= 65 && str[i - 1] <= 90) || (str[i - 1] >= 97 && str[i - 1] <= 122)))
             i++;
-        }
-        else if ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+        else if ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
             break;
+        write(1, &str[i], 1);
+        i++;
     }
     write(1, "\n", 1);
 }
